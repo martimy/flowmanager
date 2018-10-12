fdeeb07af31dcbc88183e6ce98de2b045c59e06b
Maen <artimym@gmail.com>
2018-10-12 13:26:53 -0300

    fixing a bug in line 66

--- webapi.py
+++ webapi.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2018 Maen Artimy
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -58,14 +58,14 @@ class WebApi(ControllerBase):
     def get_switch_data(self, req, **_kwargs):
         """Get switch data
         """
-        if req.GET: # is this if needed? 
+        if req.GET: # is this if needed?
             lst = {} # the server always returns somthing??
             if req.GET.get("list") == "switches":
                 lst = {t[0]:t[0] for t in self.api.get_switches()}
             else:
-                request = req.GET.keys()[0]
+                request = list(req.GET.keys())[0]
                 dpid = int(req.GET[request])
-                lst = self.api.get_stats_request(request, dpid) 
+                lst = self.api.get_stats_request(request, dpid)
 
             res = Response(content_type="application/json")
             res.json = lst
@@ -79,12 +79,12 @@ class WebApi(ControllerBase):
         res = Response(content_type="application/json")
         reply = self.api.get_topology_data()
         res.json = reply
-        return res       
+        return res
 
     @route('monitor', '/meterform', methods=['POST'])
     def post_meter_form(self, req, **_kwargs):
         """Connect with meter form
-        """ 
+        """
         if req.POST:
             res = Response()
             res.body = self.api.process_meter_message(req.json)
@@ -94,7 +94,7 @@ class WebApi(ControllerBase):
     @route('monitor', '/groupform', methods=['POST'])
     def post_group_form(self, req, **_kwargs):
         """Connect with group form
-        """ 
+        """
         if req.POST:
             res = Response()
             res.body = self.api.process_group_message(req.json)
@@ -104,7 +104,7 @@ class WebApi(ControllerBase):
     @route('monitor', '/flowform', methods=['POST'])
     def post_flow_form(self, req, **_kwargs):
         """Connect with flow control form
-        """ 
+        """
         if req.POST:
             res = Response()
             res.body = self.api.process_flow_message(req.json)
@@ -114,7 +114,7 @@ class WebApi(ControllerBase):
     @route('monitor', '/upload', methods=['POST'])
     def post_config_upload(self, req, **_kwargs):
         """Connect with configuration upload form
-        """ 
+        """
         if req.POST:
             meters = req.json.get('meters', None)
             groups = req.json.get('groups', None)
@@ -126,13 +126,13 @@ class WebApi(ControllerBase):
             res = Response()
             res.body = "{}, {}, {}".format(rm, gm, fm)
             return res
-            
+
         return Response(status=400)  # bad request
 
     @route('monitor', '/flowdel', methods=['POST'])
     def post_flow_delete(self, req, **_kwargs):
         """Receive flows delete request
-        """ 
+        """
         if req.POST:
             res = Response()
             res.body = self.api.delete_flow_list(req.json)
@@ -173,7 +173,7 @@ class WebApi(ControllerBase):
     # def get_log_SSE(self, req, **_kwargs):
     #     # Support for SSE
     #     # https://streamdata.io/blog/server-sent-events/
-       
+
     #     def eventStream():
     #         print("entered")
     #         while len(self.messages) > 0:
