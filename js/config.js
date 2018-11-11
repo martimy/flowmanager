e6aa25b6e227f8fa27f05f397fe0ee46a44b9fb9
Maen Artimy <artimym@gmail.com>
2018-11-11 13:17:54 -0400

    fixing a bug in restoring meter and group configs

--- js/config.js
+++ js/config.js
@@ -16,31 +16,31 @@ $(function () {
 
   var $form = $('#confbackup');
   var all_switches = [];
-  var request = {"meterupload":"meters", "groupupload":"groups", "flowupload":"flows"}
-  
+  var request = { "meterupload": "meters", "groupupload": "groups", "flowupload": "flows" }
+
   // Get information from datapaths
   function getSwitchData(request, switches, g) {
-    if($.isEmptyObject(switches)) {
+    if ($.isEmptyObject(switches)) {
       g([]);
     }
     var lst = [];
     var all_data = [];
     // Request flows from all switches
-    for(var sw in switches) {
+    for (var sw in switches) {
       lst.push(
-          $.get("/status", {status:request, dpid:switches[sw]})
-          .done( function(flows) {
-              all_data.push(flows)
+        $.get("/status", { status: request, dpid: switches[sw] })
+          .done(function (flows) {
+            all_data.push(flows)
           })
-          .fail( function() {
-              var msg = "Cannot read " + request + " form " + switches[sw] + "!";
-              displayMessage(msg);
+          .fail(function () {
+            var msg = "Cannot read " + request + " form " + switches[sw] + "!";
+            displayMessage(msg);
           })
       )
     }
 
     // Wait for all switches to reply 
-    $.when.apply(this,lst).then(function() {
+    $.when.apply(this, lst).then(function () {
       g(all_data);
     });
 
@@ -49,7 +49,7 @@ $(function () {
   // When Enter is pressed, go to the next input instead of sumbitting
   // the form.
   $form.on('keypress', ':input', function (e) {
-    if(e.which == 13) {
+    if (e.which == 13) {
       e.preventDefault();
       var $canfocus = $(':input');
       var index = $canfocus.index(this) + 1;
@@ -61,37 +61,37 @@ $(function () {
 
   // Handle 'change' events for the match checkbox
   $('#matchcheckbox').on('change', function (e) {
-    if(this.checked) {
+    if (this.checked) {
       $('#matchdiv').slideUp(100); //addClass('hidden'); //fadeOut(250);
     } else {
       $('#matchdiv').slideDown(100); //removeClass('hidden'); //fadeIn(250);
     }
   });
-  
+
   // Display Snackbar
   // TODO: move it to a common file
   function displaySnackbar(msg) {
     var $x = $("#snackbar");
     $x.text(msg)
     $x.toggleClass("show");
-    setTimeout(function(){ $x.toggleClass("show"); }, 3000);
+    setTimeout(function () { $x.toggleClass("show"); }, 3000);
   }
 
   // Check if the file is valid backup file
   function validFile(data) {
-    return data.indexOf("FLOWMANAGER_v1.0")>=0;
+    return data.indexOf("FLOWMANAGER_v1.0") >= 0;
   }
 
   $('[name="chooseFile"]').bind('change', function () {
     //var $form = $(this).closest('form');
     var filename = $(this).val();
-    if(!/(\.bk)$/i.test(filename)) { 
+    if (!/(\.bk)$/i.test(filename)) {
       $form.find(".file-upload").removeClass('active');
       $form.find(".file-select-name").text("No file chosen...");
       $form.find(".filecontent").val("No content...");
       displaySnackbar("Invalid Extension!")
     }
-    else {    
+    else {
       $form.find(".file-upload").addClass('active');
       $form.find(".file-select-name").text(filename.replace("C:\\fakepath\\", ""));
 
@@ -101,12 +101,12 @@ $(function () {
       var inputFile = inputFiles[0];
 
       var reader = new FileReader();
-      reader.onload = function(event) {
+      reader.onload = function (event) {
         $input.val(event.target.result);
         //console.log(event.target.result);
       };
-      reader.onerror = function(event) {
-        displaySnackbar("Error "+event.target.error.code);
+      reader.onerror = function (event) {
+        displaySnackbar("Error " + event.target.error.code);
       };
       reader.readAsText(inputFile);
       $form.find(".file-submit").prop('disabled', false);
@@ -114,23 +114,23 @@ $(function () {
   });
 
   // Handle form 'submit' events
-  $form.on('click', '[name="restore"]', function(e) {
+  $form.on('click', '[name="restore"]', function (e) {
     e.preventDefault();
     var $input = $form.find(".filecontent");
     var data = $input.val();
 
-    if(!data | !validFile(data)) {
+    if (!data | !validFile(data)) {
       displaySnackbar("Invalid file format!");
       return;
     }
 
     //Send the data to the server
     $.post("/upload", data)
-      .done( function(response) {
+      .done(function (response) {
         var msg = response;
         displaySnackbar(msg);
       })
-      .fail( function() {
+      .fail(function () {
         var msg = "No response from controller.";
         displaySnackbar(msg);
       })
@@ -146,34 +146,46 @@ $(function () {
     getSwitchData(
       "meters",
       switches,
-      function(all_data) {
-        conf["meters"] = JSON.parse(fix_compatibility(JSON.stringify(all_data)));
+      function (all_data) {
+        var data = [];
+        for (d in all_data) {
+          data.push(all_data[d]["desc"]);
+        }
+        conf["meters"] = JSON.parse(fix_compatibility(JSON.stringify(data)));
         getSwitchData(
           "groups",
           switches,
-          function(all_data) {
-            conf["groups"] = JSON.parse(fix_compatibility(JSON.stringify(all_data)));
+          function (all_data) {
+            var data = [];
+            for (d in all_data) {
+              data.push(all_data[d]["desc"]);
+            }
+            conf["groups"] = JSON.parse(fix_compatibility(JSON.stringify(data)));
             getSwitchData(
               "flows",
               switches,
-              function(all_data) {
+              function (all_data) {
                 conf["flows"] = JSON.parse(fix_compatibility(JSON.stringify(all_data)));
-                conf = JSON.stringify(conf);
-                var filename = "backup_"+Date.now()+".bk"
+                if ($('#pretty').is(':checked')) {
+                  conf = JSON.stringify(conf, undefined, 2);
+                } else {
+                  conf = JSON.stringify(conf);
+                }
+                var filename = "backup_" + Date.now() + ".bk"
                 downloadFile(filename, conf);
               }
-            );        
+            );
           }
-        );    
+        );
       }
     );
   }
-  
-  $form.on('click', '[name="save"]', function(e) {
+
+  $form.on('click', '[name="save"]', function (e) {
     e.preventDefault();
     var switches = [];
-    $("#dpid option:selected").each(function(){   
-        switches.push(this.value); // or $(this).val()
+    $("#dpid option:selected").each(function () {
+      switches.push(this.value); // or $(this).val()
     });
     switches = switches.includes("ALL") ? all_switches : switches;
     getConfiguration(switches);
@@ -182,20 +194,19 @@ $(function () {
   // Get list of active datapaths
   function getActiveSwitches() {
     // Get switches list
-    $.get("/data","list=switches")
-    .done( function(response) {
-      var $dps = $('#dpid');
-      for (var i in response) {
-        all_switches.push(response[i]);
-        $dps.append('<option value="' + response[i] + '">SW_' + response[i] + '</option>')
-      }
-    })
-    .fail( function() {
-      $('#output').text("Cannot read switches!");
-    })
+    $.get("/data", "list=switches")
+      .done(function (response) {
+        var $dps = $('#dpid');
+        for (var i in response) {
+          all_switches.push(response[i]);
+          $dps.append('<option value="' + response[i] + '">SW_' + response[i] + '</option>')
+        }
+      })
+      .fail(function () {
+        $('#output').text("Cannot read switches!");
+      })
   }
 
   getActiveSwitches();
 })
 
-  
